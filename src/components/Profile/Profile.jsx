0b078e90bfbaf2cd34bce8d157e90aec91ace3d9
Vladyslav <168086670+VladyslavPikuza@users.noreply.github.com>
2024-10-26 00:59:40 +0300

    212

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+
+import s from './Profile.module.css';
+
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={s.container}>
+      <div className={s.card}>
+        <img className={s.image} src={image} alt="User avatar" />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.list}>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
+
+
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import  React  from  'react' ;
  import  ReactDOM  from   'react-dom/client' ;
 import App from './components/App.jsx';
 import 'modern-normalize';
- import  './index.css' ;
+import './index.css';
+
 
 ReactDOM.createRoot ( document . getElementById ( 'root' )). render (
    < React.StrictMode > 
