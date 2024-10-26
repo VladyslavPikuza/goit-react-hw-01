c063110e5a58ef2d022524d045bffd380ac4b00c
Vladyslav <168086670+VladyslavPikuza@users.noreply.github.com>
2024-10-26 03:03:54 +0300

    3123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import Profile from './Profile/Profile';
 import userData from './../userData.json';
 import FriendList from './FriendList/FriendList';
 import friends from './../friends.json';
+import transactions from './../transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 const App = () => {
   return (
@@ -13,7 +15,8 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
           />
-           <FriendList friends={friends} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
